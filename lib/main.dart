80772d8452a90ece0bce87625b6b145c8a5d1557
unknown <sameeraalahmad112@gmail.com>
2024-03-19 13:43:27 +0300

    push#6

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,15 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: const FirebaseOptions(
-      apiKey: "AIzaSyAyeXqzbl16wE0oVOGBjc20fotvYdtZoG0", // paste your api key here
-      appId: "1:194857567449:android:678be1e2de2560e184eac8", //paste your app id here
+      apiKey:
+          "AIzaSyAyeXqzbl16wE0oVOGBjc20fotvYdtZoG0", // paste your api key here
+      appId:
+          "1:194857567449:android:678be1e2de2560e184eac8", //paste your app id here
       messagingSenderId: "194857567449", //paste your messagingSenderId here
       projectId: "mhealthapp-2647c", //paste your project id here
-    ),);
-  runApp( MaterialApp(
+    ),
+  );
+  runApp(const MaterialApp(
     home: Scaffold(
       body: LoginPage(),
     ),
